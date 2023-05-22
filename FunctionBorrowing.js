aebdfe7676e719a45c32f54d9058000afe57241e
Vishal Srivastava <eth.vishal@gmail.com>
2023-05-22 12:45:26 +0530

    Added examples of apply and bind method

--- FunctionBorrowing.js
+++ FunctionBorrowing.js
@@ -1,16 +1,42 @@
+//function borrowing examples
+
+// const name = {
+//     firstName: "vishal",
+//     lastName: "Srivastava",
+//     printDetails: function () {
+//         console.log(this.firstName + " " + this.lastName)
+//     }
+// }
+
+// name.printDetails()
+
+// const name1 = {
+//     firstName: "Amit",
+//     lastName : "sana"
+// }
+
+// name.printDetails.call(name1)
+
 const name = {
     firstName: "vishal",
     lastName: "Srivastava",
-    printDetails: function () {
-        console.log(this.firstName + " " + this.lastName)
-    }
 }
 
-name.printDetails()
+function printDetails (hometown) {
+    console.log(this.firstName + " " + this.lastName + " from " + hometown)
+}
+// printDetails.call(name,"agra")
 
 const name1 = {
     firstName: "Amit",
     lastName : "sana"
 }
 
-name.printDetails.call(name1)
+// printDetails.call(name1,"allahabad")
+// printDetails.apply(name1,['allahabad'])
+
+
+const print = printDetails.bind(name1,'allahabad')
+// console.log(print)
+console.log(print())
+// console.log('hello')
